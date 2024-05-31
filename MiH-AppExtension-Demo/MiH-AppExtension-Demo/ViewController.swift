6a735a4d334009b221ce6a2a0589fe533ca13deb
Riccardo Cecchini <16149805+cekkr@users.noreply.github.com>
2024-05-31 11:06:58 +0200

    base commit

---
+++ MiH-AppExtension-Demo/MiH-AppExtension-Demo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MiH-AppExtension-Demo
+//
+//  Created by Riccardo Cecchini on 31/05/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
