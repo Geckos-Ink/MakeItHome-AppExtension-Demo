ee5ddb198dc48e4a2e8eb00d600bbd2aef0252de
Riccardo Cecchini <16149805+cekkr@users.noreply.github.com>
2024-05-31 12:37:38 +0200

    post request

--- MiH-AppExtension-Demo/MiH-AppExtension-Demo/AppExtensionHTTPClient.swift
+++ MiH-AppExtension-Demo/MiH-AppExtension-Demo/AppExtensionHTTPClient.swift
@@ -14,6 +14,47 @@ class AppExtensionHTTPClient {
         return url
     }
     
+    func makePostRequest(url: URL, json: [String: Any]) -> URLRequest{
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+
+        // Create the JSON data to be sent in the request body
+        let jsonData = try? JSONSerialization.data(withJSONObject: json, options: [])
+
+        // Add the JSON data to the request body
+        request.httpBody = jsonData
+        
+        return request
+
+        /* USAGE EXAMPLE
+        // Create a URLSession data task to send the request
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            // Handle the response or error
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                print("Invalid response")
+                return
+            }
+            
+            if let data = data {
+                do {
+                    // Parse the JSON response data
+                    let jsonResponse = try JSONSerialization.jsonObject(with: data, options: [])
+                    print("Response JSON: \(jsonResponse)")
+                } catch {
+                    print("Failed to parse JSON response: \(error)")
+                }
+            }
+        }
+         */
+    }
+    
     func connect(bundleId:String){
         let url = makeRequest(path: "/connect?bundleId="+bundleId)
 
